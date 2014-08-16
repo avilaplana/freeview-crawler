a0471a87a368bb49b38e50f2d237d42b9bcb844a
alvarovilaplanagarcia <avilaplana@equalexperts.com>
2014-08-16 21:52:37 +0100

    Adding Integration tests

---
+++ test/TestTvChannelParser.py
@@ -0,0 +1,22 @@
+#!/usr/local/bin/python
+
+import unittest
+import urllib2
+from tvChannelParser import parseToTVChannels
+
+class TestTvChannelParser(unittest.TestCase):
+
+    def setUp(self):
+        self.seq = range(10)
+
+    def test_shuffle(self):
+        tv_channels_url = 'http://tvlistings.theguardian.com/text-only'
+        channels_html_loaded = urllib2.urlopen(tv_channels_url).read()
+        list_channels = parseToTVChannels(channels_html_loaded)
+        for channel in list_channels:
+            self.assertTrue(channel.channel_document["name"] is not "", "name in channel does not exist")
+            self.assertTrue(channel.channel_document["language"] is not "", "language in channel does not exist")
+            self.assertTrue("/?c=" in channel.query_parameter, "query parameter does not exist")
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test/TestTvContentParser.py
@@ -0,0 +1,39 @@
+#!/usr/local/bin/python
+
+import unittest
+import urllib2
+from tvContentParser import parseToTVContent
+
+class TestTvChannelParser(unittest.TestCase):
+
+    def setUp(self):
+        self.seq = range(10)
+
+    def test_shuffle(self):
+        tv_content_channel_url = 'http://tvlistings.theguardian.com/text-only/?c=bbc-3'
+        tv_content_channel_html_loaded = urllib2.urlopen(tv_content_channel_url).read()
+        tv_content_channel_documents = parseToTVContent("Channel 4", tv_content_channel_html_loaded)
+        for document in tv_content_channel_documents:
+            self.assertTrue(document["channel"] is not "", "channel name does not exist")
+            self.assertTrue(document["description"] is not "", "description does not exist")
+            if "flags" in document:
+                self.assertTrue(document["flags"] is not "", "flags is empty")
+            self.assertTrue(document["category"] is not "", "category does not exist")
+            self.assertIn("startTime", document, "startTime does not exist")
+            self.assertIn("endTime", document, "endTime does not exist")
+            if "program" in document:
+                self.assertTrue(document['program']['title'] is not "", "title is empty")
+            if "serie" in document:
+                self.assertTrue(document['serie']['serieTitle'] is not "", "serieTitle is empty")
+                if "seasonNumber" in document['serie']:
+                    self.assertTrue(document['serie']['seasonNumber'] is not "", "seasonNumber is empty")
+                if "episodeNumber" in document['serie']:
+                    self.assertTrue(document['serie']['episodeNumber'] is not "", "episoeNumber is empty")
+                if "totalNumber" in document['serie']:
+                    self.assertTrue(document['serie']['totalNumber'] is not "", "totalNumber is empty")
+
+                self.assertTrue(document['serie']['episodeTitle'] is not "", "episodeTitle is empty")
+
+
+if __name__ == '__main__':
+    unittest.main()
