1e12a453bb33db0ee616f5bd6c49371bd2e0f75a
alvarovilaplanagarcia <avilaplana@equalexperts.com>
2014-10-10 19:49:55 +0100

    Looping against all the tags and all the times from 12am to 10am

--- tvChannelLoader.py
+++ tvChannelLoader.py
@@ -21,32 +21,57 @@ def getChannels(url):
         list_channels.append(channel.text)
     return list_channels
 
+
+def add_type_to_channel(channels_classified, channels_by_type, key, value):
+    for channel in channels_by_type:
+        if channel not in channels_classified:
+            channels_classified[channel] = {}
+            channels_classified[channel][key] = []
+            channels_classified[channel][key].append(value)
+            channels_classified[channel]['name'] = channel
+        else:
+            if key in channels_classified[channel]:
+                channels_classified[channel][key].append(value)
+            else:
+                channels_classified[channel][key] = []
+                channels_classified[channel][key].append(value)
+    return channels_classified
+
+
 def findChannelClassifed(tags):
     for tag_url in tags:
-        if 'All' in tag_url:
-            all_channels = getChannels(tag_url)
+        # if 'All' in tag_url:
+        #     all_channels = getChannels(tag_url)
         if 'Freeview' in tag_url:
             freeview_channels = getChannels(tag_url)
         if 'Terrestrial' in tag_url:
             terrestrial_channels = getChannels(tag_url)
         if 'Sky & Cable' in tag_url:
             cable_all_channels = getChannels(tag_url)
+        if 'Films' in tag_url:
+            films_channels = getChannels(tag_url)
+        if 'Sport' in tag_url:
+            sport_channels = getChannels(tag_url)
+        if 'News & Doc' in tag_url:
+            news_channels = getChannels(tag_url)
+        if 'Kids' in tag_url:
+            kids_channels = getChannels(tag_url)
+        if 'Radio' in tag_url:
+            radio_channels = getChannels(tag_url)
 
-    channels_classified = []
-    for channel in all_channels:
-        channel_u = parseChannel(channel)
-        channel_provided_by = {}
-        channel_provided_by['name'] = channel_u
-        channel_provided_by['provider'] = []
-        if channel in freeview_channels:
-            channel_provided_by['provider'].append("FREEVIEW")
-        if channel in terrestrial_channels:
-            channel_provided_by['provider'].append("TERRESTRIAL")
-        if channel in cable_all_channels:
-            channel_provided_by['provider'].append("SKY-CABLE")
-        channels_classified.append(channel_provided_by)
+    channels_classified = {}
+    add_type_to_channel(channels_classified, freeview_channels, "provider", "FREEVIEW")
+    add_type_to_channel(channels_classified, terrestrial_channels, "provider", "TERRESTRIAL")
+    add_type_to_channel(channels_classified, cable_all_channels, "provider", "SKY & CABLE")
+
+    add_type_to_channel(channels_classified, films_channels, "category", "FILMS")
+    add_type_to_channel(channels_classified, sport_channels, "category", "SPORTS")
+    add_type_to_channel(channels_classified, news_channels, "category", "NEWS & DOCUMENTARY")
+    add_type_to_channel(channels_classified, kids_channels, "category", "KIDS")
+    add_type_to_channel(channels_classified, radio_channels, "category", "RADIO")
 
     return channels_classified
+
 
 from datetime import datetime
 
@@ -54,20 +79,21 @@ day = datetime.now().day
 month = datetime.now().month
 year = datetime.now().year
 
-tags = loadHtmlTags(year, month, day, '12am', 'All')
-channels_classified = findChannelClassifed(tags)
+hours = ['12am', '2am', '4am', '6am', '8am', '10am', '12pm', '2pm', '4pm', '6pm', '8pm', '10pm']
+channels_classified = {}
+for hour in hours:
+    tags = loadHtmlTags(year, month, day, hour, 'All')
+    channels_classified_temp = findChannelClassifed(tags)
+    print '-------- ' + hour
+    for channel_temp_classified in channels_classified_temp:
+        if channel_temp_classified in channels_classified:
+            if channels_classified_temp[channel_temp_classified] != channels_classified[channel_temp_classified]:
+                print "--------  DIFFERENT"
+                print channels_classified_temp[channel_temp_classified]
+                print channels_classified[channel_temp_classified]
+        else:
+            channels_classified[channel_temp_classified] = channels_classified_temp[channel_temp_classified]
+            print channel_temp_classified + ' INSERTED'
+
 for channel in channels_classified:
-    channelCollection.insert(channel)
-
-
-
-
-
-
-
-
-
-
-
-
-
+    channelCollection.insert(channels_classified[channel])
