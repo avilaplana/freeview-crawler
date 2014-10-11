94c55a02cd8ed2cdec2358d026053169dc1b4daa
alvarovilaplanagarcia <avilaplana@equalexperts.com>
2014-10-11 22:24:39 +0100

    1. refactoring the code to take into account content with times are in different days

--- tvContentLoader.py
+++ tvContentLoader.py
@@ -3,7 +3,7 @@
 import urllib2
 from bs4 import BeautifulSoup
 from parsingLibrary import loadHtmlTags, parseChannel
-
+from datetime import timedelta
 from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
@@ -19,12 +19,23 @@ def parse_title(tv_content_html):
     return m.group(1)
 
 
-def parse_time(tv_content_html, tv_content):
+def parse_time(tv_content_html, tv_content, after_noon):
     m = re.search('<div class=\'prog_pre_header\'>(.+?)</div>', tv_content_html['onmouseover'].replace('\\', ''))
-    start = str(month) + ' ' + str(day) + ' ' + str(year) + ' ' + m.group(1).split('-')[0]
-    end = str(month) + ' ' + str(day) + ' ' + str(year) + ' ' + m.group(1).split('-')[1]
-    tv_content['start'] = datetime.strptime(start, '%m %d %Y %I.%M%p')
-    tv_content['end'] = datetime.strptime(end, '%m %d %Y %I.%M%p')
+    start_hour = m.group(1).split('-')[0]
+    end_hour = m.group(1).split('-')[1]
+    start = str(month) + ' ' + str(day) + ' ' + str(year) + ' ' + start_hour
+    end = str(month) + ' ' + str(day) + ' ' + str(year) + ' ' + end_hour
+    start_datetime = datetime.strptime(start, '%m %d %Y %I.%M%p')
+    end_datetime = datetime.strptime(end, '%m %d %Y %I.%M%p')
+
+    if not after_noon and 'pm' in start_hour:
+        start_datetime = start_datetime - timedelta(days = 1)
+    else:
+        if after_noon and 'am' in end_hour:
+            end_datetime = end_datetime + timedelta(days = 1)
+
+    tv_content['start'] = start_datetime
+    tv_content['end'] = end_datetime
 
 def parse_actors(actors):
     import numpy as np
@@ -120,15 +131,13 @@ def content_details(description_html, tv_type_content):
             else:
                 tv_type_content['description'] = description_html.strip()
 
-
-def find_content(channels_content_start_time, telegraph_url):
+def find_content(channels_content_start_time, telegraph_url, is_after_noon):
     all_content_html = urllib2.urlopen(telegraph_url).read()
 
     soup = BeautifulSoup(all_content_html)
     slots = soup.findAll("div", {"class": "channel"})
 
     content_in_interval = []
-
     for slot in slots:
         channel = slot.find("div", {"class": "channel_name"}).text
         programs = slot.findAll("div", {'class': re.compile('programme')})
@@ -136,7 +145,7 @@ def find_content(channels_content_start_time, telegraph_url):
         for program in programs:
             tv_content = {}
             tv_content["channel"] = parseChannel(channel)
-            parse_time(program, tv_content)
+            parse_time(program, tv_content, is_after_noon)
             title = parse_title(program)
             parse_content(program, title, tv_content)
 
@@ -160,17 +169,17 @@ month = datetime.now().month
 year = datetime.now().year
 
 tags = loadHtmlTags(year, month, day, '12am', 'All')
-
+channels_contenr_start_time = {}
 for tag_url in tags:
     if 'Freeview' in tag_url or 'Terrestrial' in tag_url or 'Sky & Cable' in tag_url:
         hours = ['12am', '2am', '4am', '6am', '8am', '10am', '12pm', '2pm', '4pm', '6pm', '8pm', '10pm']
         # hours = ['12am']
-        channels_contenr_start_time = {}
         for hour in hours:
             url = re.sub('[0-9]*am', hour, tag_url)
             telegraph_url = 'http://tvguideuk.telegraph.co.uk/' + url
             print telegraph_url
-            partial_content = find_content(channels_contenr_start_time, telegraph_url)
+            partial_content = find_content(channels_contenr_start_time, telegraph_url, 'pm' in hour)
+
             for content in partial_content:
                 contentCollection.insert(content)
 
