99111c45383a6ba5d75dac8164d8a2fbcae6a71c
alvarovilaplana <alvaro.vilaplana@gmail.com>
2015-01-21 22:03:35 +0000

    adding more things

--- OMDbAPIConnector.py
+++ OMDbAPIConnector.py
@@ -1,10 +1,37 @@
 #!/usr/local/bin/python
 
-import urllib2
-import json
+import urllib2, urllib
 
-print " aaa "
-request = urllib2.Request("http://www.omdbapi.com/?t=Friends&y=&r=json", headers={"Accept" : "application/json"})
-series = urllib2.urlopen(request).read()
-# data = json.load(series)
-print " sss " + str(type(series))
+import datetime
+
+from tvContentRepository import find_all_films, find_all_series
+
+def getDetails(title):
+    request = urllib2.Request("http://www.omdbapi.com/?" + title + "&y=&r=json", headers={"Accept" : "application/json"})
+    urllib2.urlopen(request).read()
+
+
+f = find_all_films()
+s = find_all_series()
+
+print "films " + str(len(f))
+print "series " + str(len(s))
+
+print datetime.datetime.now()
+for film in f:
+    t = film['title'].strip()
+    f = { 't' : t}
+    getDetails(urllib.urlencode(f))
+
+for serie in s:
+    t = serie['serieTitle'].strip().encode('utf-8')
+    f = { 't' : t}
+    getDetails(urllib.urlencode(f))
+
+print datetime.datetime.now()
+
+
+
+
+
+
