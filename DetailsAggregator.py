c270ecefc9c1f1395a63d0e378f405150c2e1866
alvarovilaplana <alvaro.vilaplana@gmail.com>
2015-01-22 19:19:45 +0000

    removing error messages

--- DetailsAggregator.py
+++ DetailsAggregator.py
@@ -3,16 +3,30 @@
 import datetime
 from tvContentRepository import find_all_films, find_all_series, aggregate_extra_content
 from omdbAPIConnector import getDetails
+from parsingLibrary import split_string_by_comma
 
 details_array = []
-def extract_details(title_key, tvcontent):
-    t = tvcontent[title_key].strip()
+def _extract_details_by_title(title, id):
     details_content = {}
-    details_content['_id'] = tvcontent['_id']
-    details_content['extra'] = getDetails(t)
+    details_content['_id'] = id
+    details_content['extra'] = getDetails(title)
     return details_content
 
 
+def extract_tvcontent_array(tvcontents, title_key):
+    for t in tvcontents:
+     try:
+        details = _extract_details_by_title(t[title_key].strip(), t['_id'])
+        if 'Error' in details['extra']:
+            print t[title_key] + " NOT FOUND"
+            continue
+        if 'Actors' in details['extra']:
+            details['extra']['Actors'] = split_string_by_comma(details['extra']['Actors'])
+        if 'Genre' in details['extra']:
+            details['extra']['Genre'] = split_string_by_comma(details['extra']['Genre'])
+        details_array.append(details)
+     except:
+         print t[title_key] + " can not be processed"
 
 f = find_all_films()
 s = find_all_series()
@@ -20,20 +34,13 @@ s = find_all_series()
 print "films " + str(len(f))
 print "series " + str(len(s))
 
-print datetime.datetime.now()
-
-# for film in f:
-#     details_array.append(extract_details('title', film))
-#
-# for serie in s:
-#     details_array.append(extract_details('serieTitle', serie))
-details_array.append(extract_details('title', f[0]))
+print "start " + str(datetime.datetime.now())
+extract_tvcontent_array(f, 'title')
+extract_tvcontent_array(s, 'serieTitle')
+print "fetched all the aggreate details " + str(datetime.datetime.now())
 for d in details_array:
-    print d['_id']
     aggregate_extra_content(d['_id'], d['extra'])
-
-
-print datetime.datetime.now()
+print "Finish the persistence" + str(datetime.datetime.now())
 
 
 
