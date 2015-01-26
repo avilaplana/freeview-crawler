03a436fcc5d9712cf6797e8f146fc5e497e4bfd9
alvarovilaplanagarcia <avilaplana@equalexperts.com>
2015-01-26 21:23:12 +0000

    21 - adding the episode inside of the series and plot
    commenting some code used for testing purposes

--- tvContentLoader.py
+++ tvContentLoader.py
@@ -77,11 +77,11 @@ def parse_series_details(tv_content_series, tv_content_series_html):
     if 'of' in episode_numbers: # u'Series 1 - Episode 2 of 6'
         episode_nummber = episode_numbers.split(' of ')[0]
         total_number = episode_numbers.split(' of ')[1]
-        tv_content_series['episodeNumber'] = episode_nummber
-        tv_content_series['totalNumber'] = total_number
+        tv_content_series['episode']['episodeNumber'] = episode_nummber
+        tv_content_series['episode']['totalNumber'] = total_number
     else: # u'Series 1 - Episode 156'
         episode_nummber = episode_numbers.split(' of ')[0]
-        tv_content_series['episodeNumber'] = episode_nummber
+        tv_content_series['episode']['episodeNumber'] = episode_nummber
 
 
 def series(description_html, serie_title, tv_content_series):
@@ -95,36 +95,41 @@ def series(description_html, serie_title, tv_content_series):
         if '- Episode ' in episode_details_html: # u'Into Spring<br>Series 1 - Episode 2 of 6<br /><br /> During March and April, ....'
             season_number = episode_details_html.split('- Episode ')[0].split('Series ')[1]
             tv_content_series['serieTitle'] = serie_title
-            tv_content_series['episodeTitle'] = episode_title
-            tv_content_series['seasonNumber'] = season_number.strip()
+            tv_content_series['episode'] = {}
+            tv_content_series['episode']['episodeTitle'] = episode_title
+            tv_content_series['episode']['seasonNumber'] = season_number.strip()
             parse_series_details(tv_content_series, episode_details_html)
         else:
             series_description = episode_details_html.split('Series ')
             if len(series_description) == 1: # u'Series 16 Compilations 2<br> Harry Hill showcases some of the best celebrity      ...'
                 tv_content_series['serieTitle'] = serie_title
-                tv_content_series['episodeTitle'] = episode_title
+                tv_content_series['episode'] = {}
+                tv_content_series['episode']['episodeTitle'] = episode_title
                 description = description_html.split('<br> ')[1]
                 a = re.findall('\. Starring.*', description)
                 if len(a) == 0:
-                    tv_content_series['description'] = description
+                    tv_content_series['episode']['episodePlot'] = description
                 else:
-                    content_details(description, tv_content_series)
+                    content_details(description, tv_content_series, 'series')
             else:
                 # u'Christmas Special 1995 - The Pageant<br>Series 5<br /><br /> Christmas special from 1995. Hyacinth gets ...'
                 season_number = episode_details_html.split('Series ')[1]
+                tv_content_series['episode'] = {}
                 tv_content_series['serieTitle'] = serie_title
-                tv_content_series['episodeTitle'] = episode_title
-                tv_content_series['seasonNumber'] = season_number.strip()
+                tv_content_series['episode']['episodeTitle'] = episode_title
+                tv_content_series['episode']['seasonNumber'] = season_number.strip()
     else:
         # example 1: Series 1<br /><br /> Episodes one to five of the comedy starring Patricia Routledge and Prunella Scales
         # example 2: Series 8<br /><br />
         if '- Episode ' not in description_html:
             tv_content_series['serieTitle'] = serie_title
             series_season = description_html_parts[0].split('Series ')[1]
-            tv_content_series['seasonNumber'] = series_season.strip()
+            tv_content_series['episode'] = {}
+            tv_content_series['episode']['seasonNumber'] = series_season.strip()
         else:  # Series 2 - Episode 7 of 14<br /><br /> Matt and Jess receive an official warning ......
             season_number = episode_html.split('- Episode ')[0].split('Series ')[1]
-            tv_content_series['seasonNumber'] = season_number.strip()
+            tv_content_series['episode'] = {}
+            tv_content_series['episode']['seasonNumber'] = season_number.strip()
             tv_content_series['serieTitle'] = serie_title
             parse_series_details(tv_content_series, episode_html)
 
@@ -132,9 +137,9 @@ def series(description_html, serie_title, tv_content_series):
         description = description_html_parts[1]
         a = re.findall('\. Starring.*', description)
         if len(a) == 0:
-            tv_content_series['description'] = description
+            tv_content_series['episode']['plotEpisode'] = description
         else:
-            content_details(description, tv_content_series)
+            content_details(description, tv_content_series, 'series')
 
 
 def title_film_details(tv_film_content, title):
@@ -147,20 +152,26 @@ def title_film_details(tv_film_content, title):
     else:
         tv_film_content['title'] = film_title
 
+def set_plot_in_content(tv_type_content, plot, type):
+    if type == 'series':
+        tv_type_content['episode']['episodePlot'] = plot
+    else: tv_type_content['plot'] = plot
 
-def content_details(description_html, tv_type_content):
+def content_details(description_html, tv_type_content, type = 'no_series'):
     import re
 
     if len(description_html) > 0:
         a = re.findall('\. Starring.*', description_html)
         if len(a) > 0:
-            tv_type_content['description'] = re.sub('\. Starring.*', '', description_html).replace('<br>', '.').strip()
+            plot = re.sub('\. Starring.*', '', description_html).replace('<br>', '.').strip()
+            set_plot_in_content(tv_type_content, plot, type)
             actors = re.sub('\. Starring ', '', a[0])
             tv_type_content['actors'] = parse_actors(actors)
         else:
             b = re.findall('\..*, starring.*', description_html)
             if len(b) > 0:
-                tv_type_content['description'] = re.sub('\..*, starring.*', '', description_html).replace('<br>', '.').strip()
+                plot = re.sub('\..*, starring.*', '', description_html).replace('<br>', '.').strip()
+                set_plot_in_content(tv_type_content, plot, type)
                 tv_type_content['category'] = re.match('\. (.*),', b[0].split(' starring ')[0]).group(1).upper()
                 ac = b[0].split(' starring ')[1]
                 if '.' in ac:
@@ -170,7 +181,8 @@ def content_details(description_html, tv_type_content):
                 else:
                     tv_type_content['actors'] = parse_actors(ac)
             else:
-                tv_type_content['description'] = description_html.replace('<br>', '.').strip()
+                plot = description_html.replace('<br>', '.').strip()
+                set_plot_in_content(tv_type_content, plot, type)
 
 def find_content_interval_by_provider(telegraph_url, is_after_noon):
     all_content_html = urllib2.urlopen(telegraph_url).read()
@@ -251,6 +263,7 @@ year = datetime.now().year
 
 # month = 1
 # year = 2015
+# day = 31
 # myDAys = [1,2,3,4,5,6,7,8,9,10]
 # for day in myDAys:
 tags = loadHtmlTags(year, month, day, '12am', 'All')
